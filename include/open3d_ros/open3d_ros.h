2046f9783f8ef59f1ca32d538f38fdbf13d70488
Nikhil Khedekar <nikhilvkhedekar@gmail.com>
2020-07-01 12:17:06 -0700

    initial commit

---
+++ include/open3d_ros/open3d_ros.h
@@ -0,0 +1,17 @@
+#ifndef OPEN3D_ROS_HPP_
+#define OPEN3D_ROS_HPP_
+
+// Open3D
+#include <Open3D/Open3D.h>
+
+// ROS
+#include <sensor_msgs/PointCloud2.h>
+
+namespace open3d_ros
+{
+    void o3d_to_ros(const open3d::geometry::PointCloud &o3d_pc, sensor_msgs::PointCloud2 &ros_pc2);
+
+    void ros_to_o3d(const sensor_msgs::PointCloud2 &ros_pc2, open3d::geometry::PointCloud &o3d_pc);
+} // namespace open3d_ros
+
+#endif /*OPEN3D_ROS_HPP_*/
