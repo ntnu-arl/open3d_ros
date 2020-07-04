867035e01093a94eab2fa897cd63bc86838d6037
Pranay Mathur <37473401+Matnay@users.noreply.github.com>
2020-07-04 10:21:31 +0530

    Add files via upload

--- include/open3d_ros/open3d_ros.h
+++ include/open3d_ros/open3d_ros.h
@@ -9,9 +9,10 @@
 
 namespace open3d_ros
 {
-    void o3d_to_ros(const open3d::geometry::PointCloud &o3d_pc, sensor_msgs::PointCloud2 &ros_pc2);
+void o3d_to_rospc(const open3d::geometry::PointCloud& pointcloud, sensor_msgs::PointCloud2& ros_pc2,
+                  std::string frame_id);
 
-    void ros_to_o3d(const sensor_msgs::PointCloud2 &ros_pc2, open3d::geometry::PointCloud &o3d_pc);
-} // namespace open3d_ros
+void rospc_to_o3d(const sensor_msgs::PointCloud2ConstPtr& ros_pc2, open3d::geometry::PointCloud& o3d_pc);
+}  // namespace open3d_ros
 
 #endif /*OPEN3D_ROS_HPP_*/
