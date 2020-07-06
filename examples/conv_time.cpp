f6257d1c0aa9d54149b4fba0f7d1b65ca933d3ec
Nikhil Khedekar <nikhilvkhedekar@gmail.com>
2020-07-06 15:55:27 -0700

    updated conv_times
    Switched to clock instead of chrono
    reinitialize the variables inside the loop for repeatability

--- examples/conv_time.cpp
+++ examples/conv_time.cpp
@@ -8,40 +8,33 @@
 // open3d_ros
 #include "open3d_ros/open3d_ros.h"
 
-// C++
-#include <chrono>
-
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "open3d_ros_example_node");
     ros::NodeHandle nh;
     ros::Rate loop_rate(2);
     ros::Publisher pubCloud = nh.advertise<sensor_msgs::PointCloud2>("pointcloud", 1);
-
-    // Example XYZRGB Pointcloud
-    sensor_msgs::PointCloud2 ros_pc2;
-    open3d::geometry::PointCloud test_o3d_pc;
-    open3d::geometry::PointCloud o3d_pc;
-    open3d::io::ReadPointCloud("src/open3d_ros/data/fragment.pcd", o3d_pc);
-
     while (ros::ok())
     {
+        // Example XYZRGB Pointcloud
+        sensor_msgs::PointCloud2 ros_pc2;
+        open3d::geometry::PointCloud test_o3d_pc;
+        open3d::geometry::PointCloud o3d_pc;
+        open3d::io::ReadPointCloud("src/open3d_ros/data/fragment.pcd", o3d_pc);
         // Conversion time from Open3D PointCloud to sensor_msgs::PointCloud2
-        auto start_o3d_ros = std::chrono::steady_clock::now();
+        // auto start_o3d_ros = std::chrono::steady_clock::now();
+        std::clock_t tic = std::clock();
         open3d_ros::o3d_to_ros(o3d_pc, ros_pc2, "o3d_frame");
-        auto end_o3d_ros = std::chrono::steady_clock::now();
         std::cout << "Conversion time (o3d_to_ros) : "
-                  << std::chrono::duration_cast<std::chrono::nanoseconds>(end_o3d_ros - start_o3d_ros).count() << " ns"
+                  << float(std::clock() - tic) / CLOCKS_PER_SEC * 1000.0 << " ms"
                   << std::endl;
 
         // Conversion time from sensor_msgs::PointCloud2 to Open3D PointCloud 
         const sensor_msgs::PointCloud2ConstPtr &ros_pc2_ptr = boost::make_shared<sensor_msgs::PointCloud2>(ros_pc2);
-        auto start_ros_o3d = std::chrono::steady_clock::now();
+        std::clock_t toc = std::clock();
         open3d_ros::ros_to_o3d(ros_pc2_ptr, test_o3d_pc);
-        auto end_ros_o3d = std::chrono::steady_clock::now();
         std::cout << "Conversion time (ros_to_o3d) : "
-                  << std::chrono::duration_cast<std::chrono::nanoseconds>(end_ros_o3d - start_ros_o3d).count()
-                  << " ns \n"
+                  << float(std::clock() - toc) / CLOCKS_PER_SEC * 1000.0 << " ms \n"
                   << std::endl;
 
         // Visualize result using RViz
