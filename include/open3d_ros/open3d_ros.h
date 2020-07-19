405154c1394aa5ef264e2d1aae0f99bb7c0867d1
Nikhil Khedekar <nikhilvkhedekar@gmail.com>
2020-07-19 15:20:32 -0700

    Modified documentation

--- include/open3d_ros/open3d_ros.h
+++ include/open3d_ros/open3d_ros.h
@@ -16,22 +16,22 @@
 
 namespace open3d_ros
 {
-/// \brief Function to create a PointCloud2 from a PointCloud.
-/// Transforms a PointCloud to a sensor_msgs::PointCloud2
-/// with fields XYZ or XYZRGB depending on the input recieved
-/// \param pointcloud The input Open3D PointCloud
-/// \param ros_pc2 The ROS PointCloud2 output
-/// \param frame_id The frame of reference of the PointCloud2
+/**
+ * @brief Copy data from a open3d::geometry::PointCloud to a sensor_msgs::PointCloud2
+ * 
+ * @param pointcloud Reference to the open3d PointCloud
+ * @param ros_pc2 Reference to the sensor_msgs PointCloud2
+ * @param frame_id The string to be placed in the frame_id of the PointCloud2
  */
 void open3dToRos(const open3d::geometry::PointCloud& pointcloud, sensor_msgs::PointCloud2& ros_pc2,
                 std::string frame_id = "open3d_pointcloud");
 
-/// \brief Function to create a PointCloud from a PointCloud2.
-/// Transforms a sensor_msgs::PointCloud2 to a PointCloud
-/// with fields XYZ or XYZRGB depending on the input recieved
-/// \param ros_pc2 The input ROS PointCloud2
-/// \param o3d_pc The resultant Open3D PointCloud
-/// \param skip_colors If set to true, only XYZ fields will be transferred
+/**
+ * @brief Copy data from a sensor_msgs::PointCloud2 to a open3d::geometry::PointCloud
+ * 
+ * @param ros_pc2 Reference to the sensor_msgs PointCloud2
+ * @param o3d_pc Reference to the open3d PointCloud
+ * @param skip_colors If true, only xyz fields will be copied
  */
 void rosToOpen3d(const sensor_msgs::PointCloud2ConstPtr& ros_pc2, open3d::geometry::PointCloud& o3d_pc,
                 bool skip_colors = false);
